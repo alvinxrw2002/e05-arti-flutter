31c979a6efd2bde340e06ecc686564dae74c4393
stenly10 <yosuasaputra.stenly@gmail.com>
2022-12-02 19:02:22 +0700

    inisisasi awal leaderboard

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:e05_arti_flutter/main.dart';
 import 'package:e05_arti_flutter/UserProfile/pages/profile_page.dart';
 import 'package:e05_arti_flutter/UploadKarya/upload_karya.dart';
+import 'package:e05_arti_flutter/Leaderboard/pages/leaderboard_page.dart';
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key? key}) : super(key: key);
@@ -17,29 +18,32 @@ class NavigationDrawer extends StatelessWidget {
           ListTile(
             title: const Text('HomePage'),
             onTap: () {
-              Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context)=> const MyApp())
-              );
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const MyApp()));
             },
           ),
-
           ListTile(
             title: const Text('Profile'),
             onTap: () {
-              Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context)=> ProfilePage())
-              );
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()));
             },
           ),
-
           ListTile(
             title: const Text('Upload Karya'),
             onTap: () {
-              Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context)=> const UploadKarya())
-              );
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const UploadKarya()));
             },
           ),
+          ListTile(
+              title: const Text('Leaderboard'),
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LeaderboardPage()));
+              })
         ],
       ),
     );
